62ef604f08b06303be93f48a1eeaf5977df932aa
Jesús Soto <jesus.soto.rdz@gmail.com>
2024-02-02 16:28:08 -0600

    Conexion DB establecida

--- models/Product.cs
+++ models/Product.cs
@@ -1,6 +1,8 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
-    public class Product{
+public class Product{
+        [Key]
         public int Id { get; set; }
         public required string Name { get; set; }
         public decimal Price { get; set; }
